3a7df821a658718ae4d320d2158ba4a3a52d2661
Its-Ajua <akinladerebecca04@gmail.com>
2024-04-21 23:53:42 +0100

    add logger

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import app from "./index.js";
+import { logger } from "./lib/logger.js";
 import { PORT, MONGO_URI } from "./config/index.js";
 import { connect } from "./database/connection.js";
 
 connect(MONGO_URI).then(() => {
   console.log("Connected to DB");
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}, visit http://localhost:5000`);
+    logger.log("info",`Server is running on port ${PORT}, visit http://localhost:5000`);
   });
 }).catch((err)=> console.log(err));
